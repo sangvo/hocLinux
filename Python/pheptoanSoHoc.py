591145feb4da5fbf3a27f36a18d5ace4a891513a
Sang Vo <sangvo.info@gmail.com>
2018-04-20 08:28:22 +0700

    Upload thu muc

---
+++ Python/pheptoanSoHoc.py
@@ -0,0 +1,6 @@
+print "Nhap so a = "
+a = input()
+print "nhap so b= "
+b =  input()
+
+
---
+++ Python/tochucdulieu.py
@@ -0,0 +1,21 @@
+class SinhVien:
+
+    """Docstring for SinhVien:
+. """
+
+def __init__(self,mssv, hoten, makhoa):
+        """TODO: to be defined1. """
+        self.mssv = mssv
+        self.hoten = hoten
+        self.makhoa = makhoa
+
+        ".__init__(self)
+def prinths(self):
+        print "Mssv" , self.mss
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    pass
+        
+
